39688b9a888ceb1d546ea374cf40d4be2dfb1608
Tatenda M <tindxmathew@hotmail.com>
2022-04-17 22:16:26 +0200

    adding schema

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -2,24 +2,27 @@ import { ApolloServer } from 'apollo-server-micro';
 import "reflect-metadata";
 import { buildSchema, Resolver, Query, Arg, ObjectType, Field, ID } from 'type-graphql';
 
-@ObjectType()
-export class Dog{
-    @Field(() => ID)
-    name:string;
-}
-@Resolver(Dog)
-export class DogsResolver {
-    @Query(() => [Dog])
-    dogs(): Dog[] {
-        return [
-        {name: "Bo"},
-        {name: "Lassie"},
-        ];       
-    }
-}
+import { DogsResolver } from '../../src/schema/dogs.resolver';
+
+// @ObjectType()
+// export class Dog{
+//     @Field(() => ID)
+//     name:string;
+// }
+
+// @Resolver(Dog)
+// export class DogsResolver {
+//     @Query(() => [Dog])
+//     dogs(): Dog[] {
+//         return [
+//         {name: "Bo"},
+//         {name: "Lassie"},
+//         ];       
+//     }
+// }
 
 const schema = await buildSchema({
-    resolvers: [DogsResolver]
+    resolvers: [DogsResolver],
 })
 const server = new ApolloServer({
 schema,
@@ -31,14 +34,12 @@ export const config ={
     api: {
         bodyParser: false,
     },
-
 };
 
 //start server
-const startServer = server.start()
+const startServer = server.start();
 
 //await server and handle req and res
-
 export default async function handler(req, res){
     await startServer;
     await server.createHandler({ path: "/api/graphql"})(req, res);
