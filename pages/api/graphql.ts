dadea4db1e88f15691df4d26e477e21a7355ee2a
Tatenda M <tindxmathew@hotmail.com>
2022-04-17 18:59:41 +0200

    GraphQL

---
+++ pages/api/graphql.ts
@@ -0,0 +1,45 @@
+import { ApolloServer } from 'apollo-server-micro';
+import "reflect-metadata";
+import { buildSchema, Resolver, Query, Arg, ObjectType, Field, ID } from 'type-graphql';
+
+@ObjectType()
+export class Dog{
+    @Field(() => ID)
+    name:string;
+}
+@Resolver(Dog)
+export class DogsResolver {
+    @Query(() => [Dog])
+    dogs(): Dog[] {
+        return [
+        {name: "Bo"},
+        {name: "Lassie"},
+        ];       
+    }
+}
+
+const schema = await buildSchema({
+    resolvers: [DogsResolver]
+})
+const server = new ApolloServer({
+schema,
+
+});
+
+//dont enable the body parser
+export const config ={
+    api: {
+        bodyParser: false,
+    },
+
+};
+
+//start server
+const startServer = server.start()
+
+//await server and handle req and res
+
+export default async function handler(req, res){
+    await startServer;
+    await server.createHandler({ path: "/api/graphql"})(req, res);
+}
